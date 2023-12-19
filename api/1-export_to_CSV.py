851d4c410a5323ed7ecbd2bb50d600a0330aeb5f
EsmeraldaMar <6580@holbertonstudents.com>
2023-12-19 14:22:41 +1100

    feat: added file to export to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Records all tasks that are owned by this employee
+"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Base API URL
+    url = "https://jsonplaceholder.typicode.com"
+
+    # Fetch user data using employee ID from command line argument
+    user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
+
+    # Fetch todo items for the user
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+
+    # Set the filename for the CSV file
+    filename = "{}.csv".format(user["id"])
+
+    # Open the CSV file for writing
+    with open(filename, mode="w", newline="") as csv_file:
+        # Create a CSV writer object
+        write = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        # Write headers to the CSV file
+        write.writerow(["USER_ID", "USERNAME",
+                        "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        # Loop through each todo item and write to the CSV file
+        for task in todos:
+            write.writerow([user["id"], user["username"],
+                            task["completed"], task["title"]])
